fb9992578abedb4e2bfe6110f3a5147e00a3eaa9
moerwald <andreas.mewald@gmail.com>
2018-06-24 12:36:35 +0200

    Add project files.

---
+++ Moerwald.Specflow.Extension.Storage/TableParser.cs
@@ -0,0 +1,22 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Moerwald.Specflow.Storage
+{
+    public class TableParser
+    {
+        private Table table;
+
+        public TableParser StoreValues { get { return this; } }
+
+        public TableParser From(Table table)
+        {
+            this.table = table;
+            return this;
+        }
+
+        public void In (Storage storage)
+        {
+
+        }
+    }
+}
